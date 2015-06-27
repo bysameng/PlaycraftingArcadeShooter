faba83a866edea3f0d449f2a2264bd000eead49a
Samuel Eng <sam-eng@icloud.com>
2015-06-27 12:59:18 -0400

    mechanics, spawning, title screen

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,25 +4,35 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 	
 	public GameObject enemy;
+	public float spawnDelay = .5f;
+	private float spawnTimer = 0f;
+
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		InvokeRepeating ("SpawnEnemy", 0f, 2f);
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		
+	void Update (){
+		SpawnLogic();
+	}
+
+	void SpawnLogic(){
+		if (spawnTimer > 0){
+			spawnTimer -= Time.deltaTime;
+		}
+		if (spawnTimer <= 0){
+			spawnTimer = spawnDelay;
+		}
 	}
 	
-	void SpawnEnemy ()
-	{
-		var _enemy = Instantiate(enemy, transform.position + new Vector3(0, Random.Range (-1, -10), 0), Quaternion.identity) as GameObject;
-		int rand = Random.Range (0, 2);
-		
-		if (rand == 0)
-			_enemy.transform.eulerAngles = new Vector3 (0, 0, 90);
-		
+	void SpawnEnemy (){
+		Vector3 randomVec = Random.onUnitSphere;
+		randomVec.z = 0;
+		var _enemy = Instantiate(enemy, transform.position + randomVec * 8f, Quaternion.identity) as GameObject;
+//		int rand = Random.Range (0, 2);
+//		
+//		if (rand == 0)
+//			_enemy.transform.eulerAngles = new Vector3 (0, 0, 90);
 	}
 }
--- Assets/Scripts/PlayerScalarOnCollision.cs
+++ Assets/Scripts/PlayerScalarOnCollision.cs
@@ -5,7 +5,6 @@ public class PlayerScalarOnCollision : MonoBehaviour {
 
 	public ScaleSpring scalar;
 
-
 	void OnCollisionEnter2D(Collision2D other){
 		float mag = other.relativeVelocity.magnitude / 10f;
 		scalar.AddForce(new Vector3(mag, mag, mag));
